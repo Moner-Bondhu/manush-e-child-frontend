3ea9ac2b0ca76bb7918b4b38dde08a6d91d03bc2
Munem-Prionto <munem.prionto@gmail.com>
2024-05-18 11:23:54 +0600

    child ui fix

--- src/components/ChildSelection.vue
+++ src/components/ChildSelection.vue
@@ -37,44 +37,80 @@ export default {
 </script>
 
 <template>
-<div class="grid card sm:grid-cols-2 container mx-auto my-8 text-center gap-8 w-[90%]">
-    <div class="">
-                    <div class="rounded-lg hover:bg-gray-500 ease-in duration-100">
-                            <div class="py-2">
-                            <h2 class="text-2xl  rounded-t-lg py-2">Existing Child ID</h2>
-                            <input type="number" v-model="child_id" name="child_id">
-                            </div>
+  <section class="max-w-screen-lg mx-auto py-4 my-8 px-4">
+    <div class="p-4 shadow rounded-2xl md:w-[66%] mx-auto">
+      <h1 class=" text-[1rem] sm:text-[1.5rem] font-semibold mb-8">Login</h1>
 
-                            <button @click="existing()" class="text-white bg-primary hover:bg-primaryDark focus:outline-none focus:ring-4 font-medium rounded-full text-sm px-12 py-2.5 text-center mr-2 mb-2 my-2">Login</button>
-                            
-                    </div>
+      <div class="md:grid md:grid-cols-3 md:items-start md:justify-items-start mb-6">
+        <div class="md:col-span-1">
+          <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4 text-[1.15rem]" for="clientName">
+            Existing Child ID
+          </label>
+        </div>
+        <div class="md:w-full md:col-span-2">
+          <input type="number" v-model="child_id" name="child_id"
+            class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+          <button @click="existing()"
+            class="text-white bg-primary hover:bg-primaryDark focus:outline-none focus:ring-4 font-medium rounded-md	 text-sm px-12 py-2.5 text-center mr-2 mb-2 my-2 w-fill">Login</button>
+        </div>
+      </div>
     </div>
 
-    <div class="">
-                    <div class="hover:bg-gray-500 rounded-lg ease-in duration-100">
-                            <div class="py-2">
-                                <h2 class="text-2xl  rounded-t-lg py-2">Create new Profile</h2>
-                                <label for="child_name" class="block mb-2 text-sm font-medium text-gray-900">Name : </label>
-                                <input required type="text" name="child_name" id="child_name" v-model="child_name">
+  </section>
+  <section class="max-w-screen-lg mx-auto py-4 my-8 px-4">
+    <div class="p-4 shadow rounded-2xl md:w-[66%] mx-auto">
+      <h1 class=" text-[1rem] sm:text-[1.5rem] font-semibold mb-8">Create new Profile
+      </h1>
 
-                                <label for="child_age" class="block mb-2 text-sm font-medium text-gray-900">Age : </label>
-                                <input required type="number" name="child_age" id="child_age" v-model="child_age">
+      <div class="md:grid md:grid-cols-3 md:items-center md:justify-items-start mb-6">
+        <div class="md:col-span-1">
+          <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="child_name">
+            Name
+          </label>
+        </div>
+        <div class="md:w-full md:col-span-2">
+          <input required type="text" name="child_name" id="child_name" v-model="child_name"
+            class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+        </div>
+      </div>
 
-                                <label for="child_gender" class="block mb-2 text-sm font-medium text-gray-900">Gender : </label>
-                                <select name="child_gender" id="child_gender" v-model="child_gender">
-                                    <option value="male">Male</option>
-                                    <option value="female">Female</option>
-                                    <option value="other">Other</option>
-                                </select>
+      <div class="md:grid md:grid-cols-3 md:items-center md:justify-items-start mb-6">
+        <div class="md:col-span-1">
+          <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="child_age">
+            Age
+          </label>
+        </div>
+        <div class="md:w-full md:col-span-2">
+          <input required type="number" name="child_age" id="child_age" v-model="child_age"
+            class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+        </div>
+      </div>
 
-                                <br>
-                                <button @click="create()" class="text-white bg-primary hover:bg-primaryDark focus:outline-none focus:ring-4 font-medium rounded-full text-sm px-12 py-2.5 text-center mr-2 mb-2 my-2">Create Profile</button>
-                            </div>
-                            
-                    </div>
+      <div class="md:grid md:grid-cols-3 md:items-center md:justify-items-start mb-6">
+        <div class="md:col-span-1">
+          <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="child_gender">
+            Gender
+          </label>
+        </div>
+        <div class="md:w-full md:col-span-2">
+          <select
+            class="block appearance-none w-full border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            name="child_gender" id="child_gender" v-model="child_gender">
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="other">Other</option>
+          </select>
+        </div>
+        <br>
+        <button @click="create()"
+          class="text-white bg-primary hover:bg-primaryDark focus:outline-none focus:ring-4 font-medium rounded-md text-sm px-12 py-2.5 text-center mr-2 mb-2 my-2">Create
+          Profile</button>
+      </div>
     </div>
-</div>    
-  
+
+  </section>
+
+
 </template>
 
 <style scoped>
