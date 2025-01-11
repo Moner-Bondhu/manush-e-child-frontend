a2b27673ca9f0abff803d28463db5825d28eedd6
Munem-Prionto <munem.prionto@gmail.com>
2025-01-11 11:46:53 +0600

    login pg update

--- src/components/ProfileSelection.vue
+++ src/components/ProfileSelection.vue
@@ -1,21 +1,30 @@
 <script>
 import { RouterLink } from 'vue-router';
-
+{/* <h4 class="mb-2 text-[1rem] sm:text-[1.25rem] leading-[1.6875rem] sm:leading-[2.25rem] font-semibold">
+                        Find a matching counselor!<spanc class="text-red-500">*</spanc>
+                    </h4>
+                    <p class="mb-3 text-[1rem] sm:text-[1.25rem]  leading-[1.6875rem] sm:leading-[2.25rem]">
+                        Answer a few questions, and we'll connect you with the best counselor for your needs
+                    </p> */}
 
 </script>
 
 <template>
-    <div class="grid md:grid-cols-2 container mx-auto my-8 text-center gap-8 w-[90%]">
+    <h1 class="text-2xl py-4 text-center">Who is using <b>Manush E</b> </h1>
+
+    <div class="grid md:grid-cols-3 container mx-auto my-8 text-center gap-8 w-[70%]">
         <RouterLink :to="`/child/initiate`">
             <div class="shadow rounded-2xl">
-                <div class="rounded-lg hover:bg-[#f2684e] hover:text-white ease-in duration-100 grid grid-cols-4">
+                <div class="rounded-lg hover:border hover:border-[#f2684e] ease-in duration-100 grid grid-cols-1">
                     <div class="py-2 col-span-1">
-                        <img src="https://monerbondhu-vs.s3.ap-southeast-1.amazonaws.com/monerbondhu-files/content/games/images/_bb8fcece-3ef5-4da5-91e8-b7ebc5a75ec8.jpeg"
-                            alt="" class="w-[100%] mx-auto">
+                        <img src="https://i.imghippo.com/files/DLs7447MBk.png" alt="" class="w-[100px] mx-auto px-4">
                     </div>
-                    <div class="col-span-3 text-left p-4 my-auto">
-                        <h2 class="text-2xl  rounded-t-lg font-semibold">Login as child</h2>
-                        <p class="py-4">Login as a child and add data to a child profile</p>
+                    <div class="col-span-3  px-4 my-auto">
+                        <h2
+                            class="text-[1rem] sm:text-[1.25rem]  leading-[1.6875rem] sm:leading-[2.25rem] font-semibold">
+                            Login as a <span class="text-red-500">Child</span> </h2>
+                        <p class="text-[1rem] sm:text-[1rem]  leading-[1.6875rem] pb-4">Login as a
+                            child and add data to a child profile</p>
                     </div>
 
                 </div>
@@ -23,14 +32,16 @@ import { RouterLink } from 'vue-router';
         </RouterLink>
         <RouterLink :to="`admin`">
             <div class="shadow rounded-2xl">
-                <div class="rounded-lg hover:bg-[#f2684e] hover:text-white ease-in duration-100 grid grid-cols-4">
+                <div class="rounded-lg hover:border hover:border-[#f2684e] ease-in duration-100 grid grid-cols-1">
                     <div class="py-2 col-span-1">
-                        <img src="https://monerbondhu-vs.s3.ap-southeast-1.amazonaws.com/monerbondhu-files/content/games/images/_eabc442d-be0f-4930-a3e3-594811779dc5.jpeg"
-                            alt="" class="w-[100%] mx-auto">
+                        <img src="https://i.imghippo.com/files/lSr3999Zy.png" alt="" class="w-[100px] mx-auto px-4">
                     </div>
-                    <div class="col-span-3 text-left p-4 my-auto">
-                        <h2 class="text-2xl  rounded-t-lg font-semibold">Login as Admin</h2>
-                        <p class="py-4">Login as an administrator and generate reports</p>
+                    <div class="col-span-3  px-4 my-auto">
+                        <h2
+                            class="text-[1rem] sm:text-[1.25rem]  leading-[1.6875rem] sm:leading-[2.25rem] font-semibold">
+                            Login as a <span class="text-red-500">Parent</span></h2>
+                        <p class="text-[1rem] sm:text-[1rem]  leading-[1.6875rem] pb-4">Login as an administrator and
+                            generate reports</p>
                     </div>
 
                 </div>
@@ -38,14 +49,16 @@ import { RouterLink } from 'vue-router';
         </RouterLink>
         <RouterLink :to="``">
             <div class="shadow rounded-2xl">
-                <div class="rounded-lg hover:bg-[#f2684e] hover:text-white ease-in duration-100 grid grid-cols-4">
+                <div class="rounded-lg hover:border hover:border-[#f2684e]  ease-in duration-100 grid grid-cols-1">
                     <div class="py-2 col-span-1">
-                        <img src="https://monerbondhu-vs.s3.ap-southeast-1.amazonaws.com/monerbondhu-files/content/games/images/_eabc442d-be0f-4930-a3e3-594811779dc5.jpeg"
-                            alt="" class="w-[100%] mx-auto">
+                        <img src="https://i.imghippo.com/files/fVp8495lag.png" alt="" class="w-[100px] mx-auto px-4">
                     </div>
-                    <div class="col-span-3 text-left p-4 my-auto">
-                        <h2 class="text-2xl  rounded-t-lg font-semibold">Login as Parent</h2>
-                        <p class="py-4">Login as an administrator and generate reports</p>
+                    <div class="col-span-3  px-4 my-auto">
+                        <h2
+                            class="text-[1rem] sm:text-[1.25rem]  leading-[1.6875rem] sm:leading-[2.25rem] font-semibold">
+                            Login as an <span class="text-red-500">Admin</span></h2>
+                        <p class="text-[1rem] sm:text-[1rem]  leading-[1.6875rem] pb-4">Login as an administrator and
+                            generate reports</p>
                     </div>
 
                 </div>
@@ -56,6 +69,4 @@ import { RouterLink } from 'vue-router';
 
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
